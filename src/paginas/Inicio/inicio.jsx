ff206d8482f8d6da24f5e020dcf26b7875b5b356
Fernando Rodrigues <fernando.candido@aluno.ifsp.edu.br>
2025-04-22 21:55:56 -0300

    feat: criação da pasta json e post.json

--- src/paginas/Inicio/inicio.jsx
+++ src/paginas/Inicio/inicio.jsx
@@ -1,12 +1,20 @@
 import Banner from "@/componentes/Banner";
+import styles from './Inicio.module.css';
+import posts from '@/json/posts.json'
 
-export default function Inicio() 
-{
- return (
-      <main>
-        <Banner/>
-         <h1>Olá Mundo!</h1>
-      </main>
-       
-    );
- }
+export default function Inicio() {
+   return (
+       <main>
+           <Banner />
+           <ul className={styles.posts}>
+               { posts.map( (post)=> {
+                   return (
+                       <li key={post.id}>
+                           Post pra testar ...
+                       </li>
+                   );
+               } ) }
+           </ul>
+       </main>
+   )
+}
